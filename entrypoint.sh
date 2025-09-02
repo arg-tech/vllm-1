6f4a76122439194b11bead841285ad2ea804606f
zvonimir-delas <zvonimir.delas@gmail.com>
2025-09-02 20:14:58 +0200

    Fix python name in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ set -e
 API_KEY=$(cat "/run/secrets/vllm_api_key")
 export VLLM_API_KEY=$API_KEY
 
-python -m vllm.entrypoints.openai.api_server \
+python3 -m vllm.entrypoints.openai.api_server \
   --model Qwen/Qwen3-0.6B
