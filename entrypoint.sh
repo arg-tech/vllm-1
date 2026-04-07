0efc4e26f9fa00c0a0adc71c246bddae8fad6df6
zvonimir-delas <zvonimir.delas@gmail.com>
2026-04-07 20:20:59 +0100

    Reduce reserved memory to allow for new overheads

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,5 @@ export VLLM_API_KEY=$API_KEY
 python3 -m vllm.entrypoints.openai.api_server \
   --model Qwen/Qwen3-4B \
   --dtype=half \
-  --gpu-memory-utilization 0.95 \
+  --gpu-memory-utilization 0.85 \
   --max-model-len 4096 \
